2723ed15caf529b4fd32f9ac5456a2a22257e465
Daniel Kats <daniel_kats@symantec.com>
2019-03-15 15:37:04 -0700

    fix to About link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default class Footer extends React.Component<{}, {}> {
 	public render() {
 		return (<footer>
 			<div>Created by Daniel Kats - 2019</div>
-			<div>Limitations and more information can be found <a href="/about.html">here</a></div>
+			<div>Limitations and more information can be found <a href="about.html">here</a></div>
 			<div>Source code on <a href="https://github.com/boompig/civ6-timeline">GitHub</a></div>
 		</footer>);
 	}
